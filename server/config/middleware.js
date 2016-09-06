90c5a1c83ed37ff5d774100d021e1f33203c2742
Gamerlazer <gamerlazer@gmail.com>
2016-09-06 16:49:03 -0700

    Create findOrCreate method, start implementing fbAuth

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,7 +1,36 @@
+var app = require('../server.js');
+var keys = require('../../keys.js');
+
 var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 module.exports = {
   passport: function() {
-    
-  }
+    passport.use(new FacebookStrategy({
+      clientID: keys.facebook.FACEBOOK_APP_ID,
+      clientSecret: keys.facebook.FACEBOOK_APP_SECRET,
+      callbackURL: 'http://www.example.com/auth/facebook/callback'
+    })
+   );
+  },
 };
+
+
+  // facebook: {
+  //   FACEBOOK_APP_ID: 237844133279066,
+  //   FACEBOOK_APP_SECRET: 'fdccb8d1e3e64f1e16ae793d05470607'
+  // }
+
+/********************** NOTES **************************/
+
+/*
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/login',
+                                   failureFlash: 'Invalid username or password.' }));
+- Login in valid to / not valid to login
+
+
+{ failureFlash: 'Invalid username or password.' });
+
+*/
