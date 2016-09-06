a58c9cd47611f3d2e655d7a35c5a6b1880d37e25
Nathanael Ligon <nathanaelligon@gmail.com>
2016-09-06 15:03:13 -0700

    Implement express static middleware serving /public directory.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
+require('./config/middleware.js')(app, express);
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log('SmartNews server listening on port 3000.');
 });
 
