fcc38a465da7d8ddb788eafc4b4ea95a2803030d
Gamerlazer <gamerlazer@gmail.com>
2016-09-08 14:18:51 -0700

    Refactor limit results function

--- server/news-apis/google-trends-helpers.js
+++ server/news-apis/google-trends-helpers.js
@@ -5,9 +5,7 @@ var hotTrends = function(res, limit, country) {
   country = country || 'US';
   googleTrends.hotTrends(country)
     .then(function (response) {
-      var limitedResponse = response.slice(limit);
-      console.log(limitedResponse);
-      res.send(limitedResponse);
+      res.send(response.slice(limit));
     })
     .catch(function(error) {
       console.log(error, 'ERROR! WITH GOOGLE TRENDS');
