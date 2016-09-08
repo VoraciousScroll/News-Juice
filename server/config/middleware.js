330567288737dfd614282b15d43dc1692144a84f
Sean <shonum@gmail.com>
2016-09-07 21:15:32 -0700

    Modulize server code and get relevant data from aylien

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -6,15 +6,15 @@ module.exports = function(app, express) {
   app.use(morgan('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}));
-  app.use(session({ 
+  app.use(session({
     secret: 'juice juice',
     saveUninitialized: false,
     resave: true,
   }));
-  app.use(function(req, res, next) {
-    console.log(req.session, '<--------- Session');
-    next();
-  });
+  // app.use(function(req, res, next) {
+  //   console.log(req.session, '<--------- Session');
+  //   next();
+  // });
   app.use(express.static(__dirname + '/../../public'));
 };
 
---
+++ server/news-apis/aylien-helpers.js
@@ -0,0 +1,41 @@
+var aylienKeys = require('../../keys.js').aylien;
+var AylienNewsApi = require('aylien-news-api');
+
+/************* AYLIEN API HELPERS ********************/
+
+// Instantiate AylienNewsApi model
+var api = new AylienNewsApi.DefaultApi();
+
+// Configure API ID: app_id
+var app_id = api.apiClient.authentications['app_id'];
+app_id.apiKey = aylienKeys.app_id;
+
+// Configure API key: app_key
+var app_key = api.apiClient.authentications['app_key'];
+app_key.apiKey = aylienKeys.app_key;
+
+var timelineData = function(input, res) {
+
+  // more options here: https://newsapi.aylien.com/docs/endpoints/time_series/nodejs
+
+  var opts = {
+    'title': input,
+    'language': ['en'],
+    'publishedAtStart': 'NOW-7DAYS',
+    'publishedAtEnd': 'NOW'
+  };
+
+  api.listTimeSeries(opts, function(err, data) {
+    if (err) {
+      console.log('<------ERROR--------->', err);
+    } else {
+      console.log('API called successfully. Returned data: ' + data);
+      res.send(data);
+    }
+  });
+};
+
+
+module.exports = {
+  timelineData: timelineData
+};
