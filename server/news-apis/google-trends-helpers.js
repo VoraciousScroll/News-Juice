19787c96a59be0157b95d3bf05d4b531680af572
Gamerlazer <gamerlazer@gmail.com>
2016-09-08 14:20:44 -0700

    Update commit of googleTrends function interface

--- server/news-apis/google-trends-helpers.js
+++ server/news-apis/google-trends-helpers.js
@@ -1,7 +1,11 @@
 var googleTrends = require('google-trends-api');
 
+/************************ GOOGLE TRENDS **********************************/
 
 var hotTrends = function(res, limit, country) {  
+// hotTrends pull top # of trends from specified country
+  // resultLimit: Number
+  // country: String, ex: 'US', default is US
   country = country || 'US';
   googleTrends.hotTrends(country)
     .then(function (response) {
