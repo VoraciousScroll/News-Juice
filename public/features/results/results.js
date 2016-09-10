ea2e0a2fc3a7c9ef0c1639a7b3d14068248bb7fe
Gamerlazer <gamerlazer@gmail.com>
2016-09-09 22:09:53 -0700

    Start work on reader div

--- public/features/results/results.js
+++ public/features/results/results.js
@@ -3,15 +3,13 @@ angular.module('smartNews.results', [])
 .controller('ResultsCtrl', function($scope, $stateParams, $http) {
 
   $scope.articleReceived = $stateParams.articleReceived;
+  // $scope.articleReceived = false;
   $scope.test = 'This is the Results View';
 
-  $scope.getArticle = function(){
+  $scope.getArticle = function() {
 
     var input = $stateParams.input;
-
-    // var publishStart = '2016-03-19T22:53:43.757Z';
     var publishStart = 'NOW-2DAYS';
-    // var publishEnd = '2016-03-26T22:53:43.757Z';
     var publishEnd = 'NOW';
 
     var url = '/seearticle?input=' + input + '&start=' + publishStart + '&end=' + publishEnd;
