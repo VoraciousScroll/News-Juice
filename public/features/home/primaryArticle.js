734e2583885d269d47ba8c83dd19cbd8575bba13
Sean <shonum@gmail.com>
2016-09-10 23:28:29 -0700

    Save articles to backend and hide button when not authenticated

--- public/features/home/primaryArticle.js
+++ public/features/home/primaryArticle.js
@@ -1,6 +1,25 @@
 angular.module('smartNews.home')
 
-.controller('PrimaryArticleCtrl', function($scope, TopTrendsFactory) {
+.controller('PrimaryArticleCtrl', function($scope, TopTrendsFactory, saveArticle, isAuth) {
+
   $scope.news = TopTrendsFactory.primaryArticle;
+
+  $scope.isAuth = function(){
+    $scope.user = isAuth();
+    return !!isAuth();
+  };
+
+  $scope.clickSave = function(){
+    var article = {
+      title: $scope.news[0].title,
+      author: $scope.news[0].author.name,
+      publishDate: $scope.news[0].publishedAt,
+      articleLink: $scope.news[0].links.permalink,
+      articleSource: $scope.news[0].source.name,
+      img: $scope.news[0].media[0].url,
+      body: $scope.news[0].body
+    };
+    saveArticle(article);
+  };
 
 });
