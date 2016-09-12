db8b0425577a35a9437bab43f3dc262ebe7a03ca
Sean <shonum@gmail.com>
2016-09-11 21:05:10 -0700

    Set up user profile headers

--- public/features/profile/profile.js
+++ public/features/profile/profile.js
@@ -1,5 +1,9 @@
-angular.module('smartNews.profile', [])
+angular.module('smartNews.profile', ['smartNews.services'])
 
-.controller('ProfileCtrl', ['$scope', function($scope){
-  $scope.profile = 'Hi this is the profile page.'
+.controller('ProfileCtrl', ['$scope', 'isAuth', function($scope, isAuth){
+
+  $scope.profile = 'Hi this is the profile page.';
+
+  $scope.user = isAuth();
+
 }]);
